3136dc0db3c7dbc31dba489e18fc50077f641350
neelimapriya <neelimasultana6@gmail.com>
2023-09-19 01:02:09 +0600

    know about axios and use this

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import LineChart from "./Components/LineChart/LineChart";
 import Navbar from "./Components/Navbar/Navbar";
+import Phones from "./Components/Phones/Phones";
 import PriceOption from "./Components/priceOptions/PriceOptions";
 
 // import NavbarDaisy from "./Components/NavbarDaisy/NavbarDaisy";
@@ -12,7 +13,8 @@ function App() {
     {/* <NavbarDaisy></NavbarDaisy> */}
       <h1 className="text-7xl bg-red-700">Vite + React</h1>
       <PriceOption></PriceOption>
-      <LineChart></LineChart>
+      <LineChart ></LineChart>
+      <Phones></Phones>
 
     
     </>
